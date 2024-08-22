862ec4b3b39288a034e61fa6884632e152f4d495
Mafuja Akhtar <167004066+MafujaA@users.noreply.github.com>
2024-08-22 16:30:32 +0930

    Update to readme.md

--- app.js
+++ app.js
@@ -9,3 +9,10 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+let scope = 5;
+
+while (scope > 0) {
+  console.log('Task remaining ${scope}');
+  scope = scope -1;
+  
+}
